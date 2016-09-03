e27724dffb1fa5a5912b05b8cd39cfc015dde14e
safari029 <zero123zero321@gmail.com>
2016-09-03 19:28:06 +0900

    Released 1.0

---
+++ pzaif/__init__.py
@@ -0,0 +1,1 @@
+from pzaif import pzaif
--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@ def main():
         setup(name='pzaif',
               version='1.0',
               author="safari029",
-              py_module='pzaif',
-              packages=find_packages("src"),
+              packages=find_packages(),
               url=["https://github.com/safari029/pzaif"],
               install_requires=['requests'],
               )
